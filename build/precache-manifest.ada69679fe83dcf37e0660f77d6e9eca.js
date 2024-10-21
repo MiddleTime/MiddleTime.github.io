489df11e5e62696a20675738c26e9036103b1639
Sergei <me@ee.ee>
2024-10-21 09:44:09 +0300

    react example

---
+++ build/precache-manifest.ada69679fe83dcf37e0660f77d6e9eca.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "e3fb7d9e59a18ccc4c1d",
+    "url": "/build/static/css/main.2f51685a.chunk.css"
+  },
+  {
+    "revision": "e3fb7d9e59a18ccc4c1d",
+    "url": "/build/static/js/main.e3fb7d9e.chunk.js"
+  },
+  {
+    "revision": "55d2f86e00a4b102d925",
+    "url": "/build/static/css/1.860c1672.chunk.css"
+  },
+  {
+    "revision": "55d2f86e00a4b102d925",
+    "url": "/build/static/js/1.55d2f86e.chunk.js"
+  },
+  {
+    "revision": "3923d73d01a4271659cc",
+    "url": "/build/static/js/runtime~main.3923d73d.js"
+  },
+  {
+    "revision": "cfa420c2d71186e173c27eaec88ce1db",
+    "url": "/build/index.html"
+  }
+];
